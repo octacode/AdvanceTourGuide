b3ab7355e7bdd14decc582320f11ace0eff93a7b
octacode <allblue.shasha@gmail.com>
2018-01-04 16:41:20 +0530

    added API keys

---
+++ app/src/main/java/life/afor/code/advancetourguide/MainActivity.java
@@ -0,0 +1,86 @@
+package life.afor.code.advancetourguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setClicks();
+    }
+
+    private void setClicks() {
+        getTopPicsButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        getFoodButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        getCafeButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        getNightLifeButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        getFunButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        getShoppingButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private Button getTopPicsButton(){
+        return (Button)findViewById(R.id.top_pics);
+    }
+
+    private Button getFoodButton(){
+        return (Button)findViewById(R.id.top_pics);
+    }
+
+    private Button getCafeButton(){
+        return (Button)findViewById(R.id.top_pics);
+    }
+
+    private Button getNightLifeButton(){
+        return (Button)findViewById(R.id.top_pics);
+    }
+
+    private Button getFunButton(){
+        return (Button)findViewById(R.id.top_pics);
+    }
+
+    private Button getShoppingButton(){
+        return (Button)findViewById(R.id.top_pics);
+    }
+
+}
